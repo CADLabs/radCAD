1aac344657c6bd0f2dc0a82a492f6350064bc101
Benjamin Scholtz <ben@bitsofether.com>
2020-09-18 09:50:22 +0200

    Performance matches Python

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::{wrap_pyfunction, wrap_pymodule};
-// use pyo3::types::{IntoPyDict, PyDict};
+use pyo3::types::{PyIterator, IntoPyDict, PyDict, PyList};
 use std::collections::HashMap;
 use rayon::prelude::*;
 // use num::Float;
@@ -37,30 +37,29 @@ fn print_psubs(psubs: &PyAny) -> PyResult<()> {
 }
 
 #[pyfunction]
-fn run(timesteps: usize, states: &PyAny, psubs: &PyAny) -> PyResult<Vec<HashMap<String, f64>>> {
-    let mut result: Vec<HashMap<String, f64>> = Vec::with_capacity(timesteps + 1);
-    let intial_state: HashMap<String, f64> = states.extract()?;
-    result.push(intial_state);
-    let psubs_: &Vec<HashMap<String, HashMap<String, &PyAny>>> = &psubs.extract()?;
-    for _timestep in 0..timesteps {
-        let previous_state: &HashMap<String, f64> = result.last().unwrap();
-        let mut next_state: HashMap<String, f64> = previous_state.clone();
-        for psub in psubs_ {
-            match psub.get("updates") {
-                None => println!("No such key \"updates\""),
-                Some(updates) => {
-                    // for (state, value) in next_state.iter_mut() {
-                    //     *value = updates.get(state).unwrap().call((previous_state.clone(),), None)?.extract()?;
-                    // }
+fn run(timesteps: usize, states: &PyDict, psubs: &PyList) -> PyResult<PyObject> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let result: &PyList = PyList::empty(py);
+    let intial_state: &PyDict = states;
+    result.append(intial_state);
+    for timestep in 0..timesteps {
+        let previous_state: &PyDict = result.get_item(timestep as isize).extract()?;
+        let next_state: &PyDict = previous_state.copy().unwrap();
+        for psub in psubs {
+            match psub.get_item("updates") {
+                Err(_e) => println!("No such key \"updates\""),
+                Ok(updates_) => {
+                    let updates: &PyDict = updates_.extract()?;
                     for (state, function) in updates {
-                        *next_state.get_mut(state).unwrap() = function.call((previous_state.clone(),), None)?.extract()?;
+                        next_state.set_item(state, function.call((next_state,), None)?);
                     }
                 }
             }
         }
-        result.push(next_state);
+        result.append(next_state);
     }
-    Ok(result)
+    Ok(result.into())
 }
 
 /// A Python module implemented in Rust.
