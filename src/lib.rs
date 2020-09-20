9453b65dd270cc1c6a94970ce627e1f51a9a9c95
Benjamin Scholtz <ben@bitsofether.com>
2020-09-20 23:01:08 +0200

    cadCAD dataframe equality test

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,21 @@
 use pyo3::prelude::*;
-use pyo3::{wrap_pyfunction, wrap_pymodule};
-use pyo3::types::{PyIterator, IntoPyDict, PyDict, PyList};
-use std::collections::HashMap;
-use rayon::prelude::*;
+use pyo3::types::{PyDict, PyList};
+use pyo3::{wrap_pyfunction};
+// use std::collections::HashMap;
+// use rayon::prelude::*;
 // use num::Float;
 
+// #[pyfunction]
+// fn state_update(func: &PyAny) -> PyResult<(String, i64)> {
+//     let result: i64 = func.call(("Hello, world!",), None)?.extract()?;
+//     Ok(("yay".to_string(), result))
+// }
 
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-#[pyfunction]
-fn py_dict() -> PyResult<HashMap<String, i32>> {
-    let mut result = HashMap::new();
-    result.insert("a".to_string(), 1);
-    Ok(result)
-}
-
-#[pyfunction]
-fn state_update(func: &PyAny) -> PyResult<(String, i64)> {
-    let result: i64 = func.call(("Hello, world!",), None)?.extract()?;
-    Ok(("yay".to_string(), result))
-}
-
-#[pymodule]
-fn wrapper(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(state_update))?;
-    Ok(())
-}
-
-#[pyfunction]
-fn print_psubs(psubs: &PyAny) -> PyResult<()> {
-    print!("{}", psubs.str().unwrap());
-    Ok(())
-}
+// #[pymodule]
+// fn wrapper(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_wrapped(wrap_pyfunction!(state_update))?;
+//     Ok(())
+// }
 
 // [{'a': 1.0,
 //   'b': 2.0,
@@ -45,40 +26,55 @@ fn print_psubs(psubs: &PyAny) -> PyResult<()> {
 //   'timestep': 0},
 
 #[pyfunction]
-fn run(timesteps: usize, states: &PyDict, psubs: &PyList) -> PyResult<PyObject> {
+fn run(timesteps: usize, runs: usize, states: &PyDict, psubs: &PyList) -> PyResult<PyObject> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let result: &PyList = PyList::empty(py);
+    for run in 0..runs {
+        result
+            .call_method(
+                "extend",
+                (single_run(timesteps, run, states, psubs)?,),
+                None,
+            )
+            .unwrap();
+    }
+    Ok(result.into())
+}
+
+#[pyfunction]
+fn single_run(timesteps: usize, run: usize, states: &PyDict, psubs: &PyList) -> PyResult<PyObject> {
     let gil = Python::acquire_gil();
     let py = gil.python();
     let result: &PyList = PyList::empty(py);
     let intial_state: &PyDict = states;
-    result.append(intial_state);
+    intial_state.set_item("simulation", 0).unwrap();
+    intial_state.set_item("subset", 0).unwrap();
+    intial_state.set_item("run", run + 1).unwrap();
+    intial_state.set_item("substep", 0).unwrap();
+    intial_state.set_item("timestep", 0).unwrap();
+    result.append(intial_state.copy()?).unwrap();
     for timestep in 0..timesteps {
-        let previous_state: &PyDict = result.get_item(timestep as isize).extract()?;
-        previous_state.set_item("simulation", 0);
-        previous_state.set_item("run", 1);
-        previous_state.set_item("subset", 0);
-        previous_state.set_item("timestep", timestep);
+        let previous_state: &PyDict = result.get_item(result.len() as isize - 1).cast_as::<PyDict>()?.copy()?;
+        previous_state.set_item("simulation", 0).unwrap();
+        previous_state.set_item("subset", 0).unwrap();
+        previous_state.set_item("run", run + 1).unwrap();
+        previous_state.set_item("timestep", timestep + 1).unwrap();
         let substeps: &PyList = PyList::empty(py);
-        let psub_iterator = psubs.into_iter();
-        for (substep, psub) in psub_iterator.enumerate() {
-            let substate = match substep {
-                0 => previous_state.copy().unwrap(),
-                _ => substeps.get_item(substep as isize - 1).clone(),
+        for (substep, psub) in psubs.into_iter().enumerate() {
+            let substate: &PyDict = match substep {
+                0 => previous_state.cast_as::<PyDict>()?.copy()?,
+                _ => substeps.get_item(substep as isize - 1).cast_as::<PyDict>()?.copy()?,
             };
-            substate.set_item("substep", substep);
-            substeps.insert(substep as isize, substate);
-            match psub.get_item("updates") {
-                Err(_e) => println!("No such key \"updates\""),
-                Ok(updates_) => {
-                    let updates: &PyDict = updates_.extract()?;
-                    for (state, function) in updates {
-                        substate.set_item(state, function.call((substate,), None)?);
-                    }
-                }
+            substate.set_item("substep", substep + 1).unwrap();
+            for (state, function) in psub.get_item("variables")?.cast_as::<PyDict>()? {
+                substate
+                    .set_item(state, function.call((substate,), None)?)
+                    .unwrap();
             }
+            substeps.insert(substep as isize, substate).unwrap();
         }
-        for substep in substeps {
-            result.append(substep);
-        }
+        result.call_method("extend", (substeps,), None).unwrap();
     }
     Ok(result.into())
 }
@@ -86,12 +82,9 @@ fn run(timesteps: usize, states: &PyDict, psubs: &PyList) -> PyResult<PyObject> 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rad_cad(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pymodule!(wrapper))?;
-
-    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
-    m.add_wrapped(wrap_pyfunction!(py_dict))?;
-    m.add_wrapped(wrap_pyfunction!(print_psubs))?;
+    // m.add_wrapped(wrap_pymodule!(wrapper))?;
     m.add_wrapped(wrap_pyfunction!(run))?;
+    m.add_wrapped(wrap_pyfunction!(single_run))?;
 
     Ok(())
 }
